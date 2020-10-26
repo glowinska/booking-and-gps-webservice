ccabe3f36eac4d7186406c232ee19642190570cd
glowinska <anna.glowinska98@gmail.com>
2020-10-26 15:13:40 +0100

    script using post method

---
+++ post_gps.py
@@ -0,0 +1,16 @@
+import http.client
+import json
+from base64 import b64encode
+import datetime
+
+conn = http.client.HTTPConnection('localhost:5000')
+userAndPass = b64encode(b"grawer:tajnePRZEZpoufne").decode("ascii")
+headers = {'Content-type': 'application/json', 'Authorization' : 'Basic %s' %  userAndPass}
+
+json_body = {'id' : 'gps', 'tid': '5', 'date' : '2021-10-15 08:22:57', 'co' : "52°8'45,6''N+21°0'50,0''E" }
+json_data = json.dumps(json_body)
+
+conn.request('POST', '/gps', json_data, headers)
+
+response = conn.getresponse()
+print(response.read().decode())
